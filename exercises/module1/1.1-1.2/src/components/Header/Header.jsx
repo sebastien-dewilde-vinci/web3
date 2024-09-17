6ccc2f9b0ac1a6266dd14538312878a72f33fde3
sebastien-dewilde-vinci <sebastien.dewilde@student.vinci.be>
2024-09-17 16:12:55 +0200

    1.1-1.2 : import image

--- exercises/module1/1.1-1.2/src/components/Header/Header.jsx
+++ exercises/module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
+import logo from "../Header/LOGO HE VINCI.png"
+
 const Header = (props) => {
     return (
       <>
+        <img src={logo} alt="logo vinci" />
         <h1>{props.course}</h1>
       </>
     )
