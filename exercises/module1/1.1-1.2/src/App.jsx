03b9a086240781448cc852a2ef95c43a002a6f29
sebastien-dewilde-vinci <sebastien.dewilde@student.vinci.be>
2024-09-17 15:51:50 +0200

    1.1-1.2 : refactor - absolute imports

--- exercises/module1/1.1-1.2/src/App.jsx
+++ exercises/module1/1.1-1.2/src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header/Header"
-import Content from "./components/Content/Content"
-import Total from "./components/Total/Total"
+import Content from "components/Content/Content"
+import Header from "components/Header/Header"
+import Total from "components/Total/Total"
 
 const App = () => {
   const course = 'Half Stack application development'
